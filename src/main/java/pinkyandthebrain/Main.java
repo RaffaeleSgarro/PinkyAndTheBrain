5b355ab0781e709aae02e8fd9f39cb1a49b2a7f4
Raffaele Sgarro <raffaelesgarro@gmail.com>
2016-02-11 22:04:14 +0100

    Print solution to file

--- src/main/java/pinkyandthebrain/Main.java
+++ src/main/java/pinkyandthebrain/Main.java
@@ -4,9 +4,7 @@ import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -70,7 +68,7 @@ public class Main {
 
         simulation.start();
 
-        PrintWriter out = new PrintWriter(System.out);
+        PrintWriter out = new PrintWriter(new FileOutputStream(new File(args[0] + ".commands")));
         out.println(simulation.getCommands().size());
 
         for (String command : simulation.getCommands()) {

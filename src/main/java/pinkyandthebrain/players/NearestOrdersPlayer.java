6c7b523e05894bcd4fbddc85a75dcedc1ca6724f
Raffaele Sgarro <raffaelesgarro@gmail.com>
2016-02-15 20:19:04 +0100

    Improved nearest order

--- src/main/java/pinkyandthebrain/players/NearestOrdersPlayer.java
+++ src/main/java/pinkyandthebrain/players/NearestOrdersPlayer.java
@@ -58,6 +58,8 @@ public class NearestOrdersPlayer implements Player, RetrieveListener {
             Item item = null;
             Warehouse warehouse = null;
             double bestDistance = Double.MAX_VALUE;
+            int availableCapacity = drone.getAvailableCapacity();
+            List<Deliver> deliverCommands = new ArrayList<>();
 
             for (Item i : unscheduled) {
                 List<Warehouse> availableWarehouses = findWarehouseWith(i.getProduct(), 1);
@@ -74,10 +76,77 @@ public class NearestOrdersPlayer implements Player, RetrieveListener {
 
             unscheduled.remove(item);
             reserve(warehouse, item.getProduct(), 1);
-
+            availableCapacity -= item.getProduct().getWeight();
             drone.submit(new Load(warehouse, item.getProduct(), 1));
-            drone.submit(new Deliver(item.getOrder(), item.getProduct(), 1));
+            deliverCommands.add(new Deliver(item.getOrder(), item.getProduct(), 1));
+
+
+            for (Item item2 : item.getOrder().getItems()) {
+                if (unscheduled.contains(item2)) {
+                    if (availableCapacity >= item2.getProduct().getWeight()) {
+                        unscheduled.remove(item2);
+                        reserve(warehouse, item2.getProduct(), 1);
+                        availableCapacity -= item2.getProduct().getWeight();
+                        drone.submit(new Load(warehouse, item2.getProduct(), 1));
+                        deliverCommands.add(new Deliver(item2.getOrder(), item2.getProduct(), 1));
+                    }
+                }
+            }
+
+
+            List<Item> item2OrderedByCost = new ArrayList<>();
+            // 0.1 is magic constant
+            double cutoff = .1 * .5* (simulation.getColumns() + simulation.getRows());
+
+            for (Item maybeAdd : unscheduled) {
+                if (maybeAdd.getOrder().getDestination().distanceTo(item.getOrder().getDestination()) < cutoff) {
+                    item2OrderedByCost.add(maybeAdd);
+                }
+            }
+
+            final Item stupidJavaItem = item;
+
+            Collections.sort(item2OrderedByCost, new Comparator<Item>() {
+                @Override
+                public int compare(Item o1, Item o2) {
+                    if (o1.getOrder().getItems().size() != o2.getOrder().getItems().size()) {
+                        return o1.getOrder().getItems().size() - o2.getOrder().getItems().size();
+                    }
+
+                    int d1 = (int) o1.getOrder().getDestination().distanceTo(stupidJavaItem.getOrder().getDestination());
+                    int d2 = (int) o2.getOrder().getDestination().distanceTo(stupidJavaItem.getOrder().getDestination());
+
+                    return d1 - d2;
+                }
+            });
+
+
+
+            for (Item item2 : item2OrderedByCost) {
+                if (findAvailableProduct(item2.getProduct(), warehouse) > 0) {
+                    if (item2.getProduct().getWeight() <= availableCapacity) {
+                        availableCapacity -= item2.getProduct().getWeight();
+                        unscheduled.remove(item2);
+                        reserve(warehouse, item2.getProduct(), 1);
+                        drone.submit(new Load(warehouse, item2.getProduct(), 1));
+                        deliverCommands.add(new Deliver(item2.getOrder(), item2.getProduct(), 1));
+                    }
+                }
+            }
+
+
+
+
+
+
+            for (Deliver cmd : deliverCommands) {
+                drone.submit(cmd);
+            }
         }
+    }
+
+    private int findAvailableProduct(Product product, Warehouse warehouse) {
+        return warehouse.queryProductQuantity(product.getId()) - reserved[warehouse.getId()][product.getId()];
     }
 
     public List<Warehouse> findWarehouseWith(Product product, int quantity) {

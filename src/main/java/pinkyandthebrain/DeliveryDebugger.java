14d601877a3248b98409b397da85b692581fb1d4
Raffaele Sgarro <raffaelesgarro@gmail.com>
2017-02-23 17:06:20 +0100

    Extracted DeliveryDebugger

---
+++ src/main/java/pinkyandthebrain/DeliveryDebugger.java
@@ -0,0 +1,11 @@
+package pinkyandthebrain;
+
+import debugger.Debugger;
+
+public class DeliveryDebugger extends Debugger {
+
+    public static void main(String... args) {
+        launch(DeliveryDebugger.class, args);
+    }
+
+}
